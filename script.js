a46abf77f269165aacb8bae4c8d83789387485bb
Sparsh-Jain-531 <sparshjain531@gmail.com>
2025-01-31 11:22:52 +0530

    Update result display and player moves in Rock-Paper-Scissors game

--- script.js
+++ script.js
@@ -14,8 +14,10 @@ let losses=document.querySelector(".losses")
 losses.innerHTML=`${score.Losses}`
 let tie=document.querySelector(".tie")
 tie.innerHTML=`${score.Ties}`
+let results=document.querySelector(".result")
+let moves=document.querySelector(".moves")
 
-const result = () => {
+const result = (playerMode) => {
 
   const modes = ["rock", "paper", "scissors"];
 
@@ -25,30 +27,40 @@ const result = () => {
 
   if (computerMode === "rock" && playerMode === "scissors") {
     score.Losses+=1;
+    results.innerHTML=`${"You Lose"}`;
   } else if (computerMode === "rock" && playerMode === "paper") {
     score.Wins+=1;
+    results.innerHTML=`${"You Win"}`;
   } else if (computerMode === "rock" && playerMode === "rock") {
     score.Ties+=1;
+    results.innerHTML=`${"Tie"}`;
   } else if (computerMode === "paper" && playerMode === "paper") {
     score.Ties+=1;
+    results.innerHTML=`${"Tie"}`;
   } else if (computerMode === "paper" && playerMode === "rock") {
     score.Losses+=1;
+    results.innerHTML=`${"You Lose"}`;
   } else if (computerMode === "paper" && playerMode === "scissors") {
     score.Wins+=1;
+    results.innerHTML=`${"You Won"}`;
   } else if (computerMode === "scissors" && playerMode === "paper") {
     score.Losses+=1;
+    results.innerHTML=`${"You Lose"}`;
   } else if (computerMode === "scissors" && playerMode === "rock") {
     score.Wins+=1;
+    results.innerHTML=`${"You Won"}`;
   } else if (computerMode === "scissors" && playerMode === "scissors") {
     score.Ties+=1;
+    results.innerHTML=`${"Tie"}`;
   } else {
     alert("Invalid input");
   }
 
   localStorage.setItem("score",JSON.stringify(score));
-  win.innerHTML=`${score.Wins}`
-  losses.innerHTML=`${score.Losses}`
-  tie.innerHTML=`${score.Ties}`
+  win.innerHTML=`${score.Wins}`;
+  losses.innerHTML=`${score.Losses}`;
+  tie.innerHTML=`${score.Ties}`;
+  moves.innerHTML=`You chooses ${playerMode} & computer chooses ${computerMode}`;
 };
 
 const resetScore=()=>{
@@ -59,19 +71,21 @@ const resetScore=()=>{
   win.innerHTML=`${score.Wins}`
   losses.innerHTML=`${score.Losses}`
   tie.innerHTML=`${score.Ties}`
+  moves.innerHTML=``
+  results.innerHTML=``;
 }
 
 document.getElementById("btn1").addEventListener("click", () => {
   playerMode = "rock";
-  result();
+  result(playerMode);
 });
 document.getElementById("btn2").addEventListener("click", () => {
   playerMode = "paper";
-  result();
+  result(playerMode);
 });
 document.getElementById("btn3").addEventListener("click", () => {
   playerMode = "scissors";
-  result();
+  result(playerMode);
 });
 document.getElementById("btn4").addEventListener("click", () => {
   resetScore()
